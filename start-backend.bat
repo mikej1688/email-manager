f7263f5b095c1ee742ead46655c385604ab3d9d6
mikej1688 <mikej1688@gmail.com>
2026-04-18 23:12:21 -0400

    removed the client_id and  client_secret out of start-backend.bat

--- start-backend.bat
+++ start-backend.bat
@@ -6,6 +6,4 @@ echo.
 echo Backend API will be available at: http://localhost:8080
 echo H2 Console: http://localhost:8080/h2-console
 echo.
-set GMAIL_CLIENT_ID=12057600958-24if33ogm2thfsm7dlnr2jt5pcfiqcha.apps.googleusercontent.com
-set GMAIL_CLIENT_SECRET=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
 call mvn spring-boot:run
